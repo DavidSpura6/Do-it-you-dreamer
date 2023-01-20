de674b9b62912585420087dbf911da752dc30383
David Špůra <davidspura6@gmail.com>
2023-01-20 09:55:00 +0100

    Add createStore lib

---
+++ src/lib/store/createStore.ts
@@ -0,0 +1,43 @@
+type StoreApi<T> = {
+  setState: (partial: T | Partial<T>) => void;
+  getState: () => T;
+  subscribe: (listener: (state: T, prevState: T) => void) => () => void;
+};
+type StateCreator<T> = (
+  setState: StoreApi<T>["setState"],
+  getState: StoreApi<T>["getState"],
+  store: StoreApi<T>
+) => T;
+type CreateStore = <T>(initializer: StateCreator<T>) => StoreApi<T>;
+
+export const createStore: CreateStore = (createState) => {
+  type TState = ReturnType<typeof createState>;
+  type Listener = (state: TState, prevState: TState) => void;
+
+  let state: TState;
+  const listeners: Set<Listener> = new Set();
+
+  const setState: StoreApi<TState>["setState"] = (partial) => {
+    const nextState = typeof partial === "function" ? partial(state) : partial;
+    if (!Object.is(nextState, state)) {
+      const previousState = state;
+      state =
+        typeof nextState !== "object"
+          ? (nextState as TState)
+          : Object.assign({}, state, nextState);
+      listeners.forEach((listener) => listener(state, previousState));
+    }
+  };
+
+  const getState: StoreApi<TState>["getState"] = () => state;
+
+  const subscribe: StoreApi<TState>["subscribe"] = (listener) => {
+    listeners.add(listener);
+    return () => listeners.delete(listener);
+  };
+
+  const api = { setState, getState, subscribe };
+  state = createState(setState, getState, api);
+
+  return api;
+};
