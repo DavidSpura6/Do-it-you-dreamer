882448d8e10223b7e499c1e0699263f943b997bc
David Špůra <davidspura6@gmail.com>
2023-02-04 10:41:32 +0100

    Update style & layout

--- src/main.ts
+++ src/main.ts
@@ -1,36 +1,26 @@
-import "./style.css";
 import "./theme/theme.css";
+import "./style.scss";
+import "./theme/layout.scss";
 import "./theme/foundations/foundations.scss";
 import typescriptLogo from "./typescript.svg";
-import { setupCounter } from "./counter";
-import { Button } from "components/Button";
-import { Checkbox } from "components/Checkbox";
+// import { setupCounter } from "./counter";
 
-const button = Button("Hello World", {
-  variant: "outlined",
-  onClick: () => console.log("click"),
-});
-const checkbox = Checkbox();
+// document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
+//   <div>
+//     <a href="https://vitejs.dev" target="_blank">
+//       <img src="/vite.svg" class="logo" alt="Vite logo" />
+//     </a>
+//     <a href="https://www.typescriptlang.org/" target="_blank">
+//       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
+//     </a>
+//     <h1>Vite + TypeScript</h1>
+//     <div class="card">
+//       <button id="counter" type="button"></button>
+//     </div>
+//     <p class="read-the-docs">
+//       Click on the Vite and TypeScript logos to learn more
+//     </p>
+//   </div>
+// `;
 
-document.body.appendChild(button);
-document.body.appendChild(checkbox);
-
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://www.typescriptlang.org/" target="_blank">
-      <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
-    </a>
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`;
-
-setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
+// setupCounter(document.querySelector<HTMLButtonElement>("#counter")!);
