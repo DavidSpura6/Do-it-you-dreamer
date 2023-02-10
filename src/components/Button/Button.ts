ab9d616ce07cd2e70bec7386395e6d2dd60554ab
David Špůra <davidspura6@gmail.com>
2023-02-10 12:24:53 +0100

    Update components

--- src/components/Button/Button.ts
+++ src/components/Button/Button.ts
@@ -5,16 +5,21 @@ type Options = {
   onClick?: (e: MouseEvent) => void;
   variant?: keyof typeof variants;
   type?: "button" | "submit";
+  icon?: string;
 };
 
 export function Button(
   html: string,
-  { variant = "solid", type = "button", onClick }: Options = {}
+  { variant = "solid", type = "button", onClick, icon }: Options = {}
 ) {
   const btn = create("button");
   btn.type = type;
   btn.innerHTML = html;
   btn.classList.add(variants[variant]);
+
+  if (variant === "icon") btn.title = html;
+  if (icon) btn.innerHTML = icon;
+
   if (onClick) btn.addEventListener("click", onClick);
   return btn;
 }
--- src/components/Checkbox/Checkbox.ts
+++ src/components/Checkbox/Checkbox.ts
@@ -1,24 +1,31 @@
-import { create } from "lib/DOM";
+import { createHtml } from "lib/DOM";
 import checkmark from "assets/done_20.svg?raw";
 import variants from "./checkbox.module.scss";
 
-export function Checkbox(ariaLabel: string, onChange?: (e: Event) => void) {
-  const label = create("label");
-  label.classList.add(variants.primary);
+export function Checkbox(
+  ariaLabel: string,
+  onChange?: (isChecked: boolean) => void,
+  { includeLabel, checked }: { includeLabel?: boolean; checked: boolean } = {
+    checked: false,
+  }
+) {
+  const html = /*html*/ `<label class="${variants.primary}">
+    <input ${
+      checked ? `checked` : ``
+    } aria-label="${ariaLabel}" type="checkbox" /> 
+    <span class="${variants.checkbox__control}">${checkmark}</span>
+    ${
+      includeLabel ? `<span class="${variants.label}" >${ariaLabel}</span>` : ``
+    }
+  </label>`;
 
-  const input = create("input");
-  input.ariaLabel = ariaLabel;
-  input.type = "checkbox";
-  input.checked = true;
-
-  const checkboxControl = create("span");
-  checkboxControl.classList.add(variants.checkbox__control);
-  checkboxControl.innerHTML = checkmark;
-
-  if (onChange)
-    input.addEventListener("input", (e) => onChange((e.target as any).checked));
-
-  label.append(input, checkboxControl);
-
-  return label;
+  const element = createHtml(html);
+  if (onChange) {
+    element
+      .querySelector("input")!
+      .addEventListener("change", (e) =>
+        onChange((e.target as HTMLInputElement).checked)
+      );
+  }
+  return element;
 }
