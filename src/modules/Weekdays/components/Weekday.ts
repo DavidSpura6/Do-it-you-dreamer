10801f1940b232e7935bb44f09d8eafe66bd1670
Lenka Vojtiskova <lenkavoj@email.cz>
2023-02-19 15:53:34 +0100

    Add reactivity to weekday button

--- src/modules/Weekdays/components/Weekday.ts
+++ src/modules/Weekdays/components/Weekday.ts
@@ -1,7 +1,8 @@
+import { read } from "fs";
 import { create } from "lib/DOM";
-import style from "./weekday.module.scss";
 import { taskStore } from "modules/stores/tasks";
 import { getSimpleDate } from "utils/simpeDate";
+import style from "./weekday.module.scss";
 
 const store = taskStore.getState();
 
@@ -10,6 +11,8 @@ export default function Weekday(day: {
   date: Date;
   dayIndex: number;
 }) {
+  const currentWeekday = getSimpleDate(new Date(day.date));
+
   const weekday = create("div");
   weekday.classList.add(style.container);
 
@@ -19,16 +22,59 @@ export default function Weekday(day: {
 
   const taskCompletion = create("div");
   taskCompletion.classList.add(style.taskCompletion);
-  taskCompletion.innerHTML = "0/4";
+
+  const tasks = taskStore.getState().tasks[currentWeekday];
+  if (tasks) {
+    const numberOfCompletedTasks = Object.values(tasks).filter(
+      (task) => task.isComplete
+    );
+    taskCompletion.innerHTML = `${numberOfCompletedTasks.length}/${
+      Object.keys(tasks).length
+    }`;
+  } else {
+    taskCompletion.innerHTML = `0/0`;
+  }
 
   weekday.append(dayName, taskCompletion);
 
-  const wrapper = create("div");
+  const wrapper = create("button");
+  wrapper.id = `date_${currentWeekday}`;
   wrapper.classList.add(style.wrapper);
   wrapper.append(weekday);
   wrapper.addEventListener("click", () => {
-    const currentWeekday = getSimpleDate(new Date(day.date));
     store.setCurrentWeekday(currentWeekday);
   });
+
+  addReactivity({
+    activeEl: wrapper,
+    numberEl: taskCompletion,
+    currentWeekday,
+  });
   return wrapper;
+}
+
+function addReactivity({
+  activeEl,
+  numberEl,
+  currentWeekday,
+}: {
+  activeEl: HTMLButtonElement;
+  numberEl: HTMLDivElement;
+  currentWeekday: string;
+}) {
+  taskStore.subscribe((state, prevState) => {
+    if (state.currentWeekday === currentWeekday) {
+      activeEl.classList.add(style.active);
+    } else {
+      activeEl.classList.remove(style.active);
+    }
+
+    const tasks = state.tasks[currentWeekday];
+    if (!tasks) return;
+    const numberOfCurrentTasks = Object.keys(tasks).length;
+    const numberOfCompletedTasks = Object.values(tasks).filter(
+      (task) => task.isComplete
+    );
+    numberEl.innerHTML = `${numberOfCompletedTasks.length}/${numberOfCurrentTasks}`;
+  });
 }
--- src/modules/Weekdays/index.ts
+++ src/modules/Weekdays/index.ts
@@ -2,10 +2,14 @@ import { getCurrentWeek } from "utils/weekdays";
 import { get } from "lib/DOM";
 import Weekday from "./components/Weekday";
 
-const container = get("#weekdays");
+const container = get("#weekdays") as Element;
 
 const week = getCurrentWeek();
 
-week.forEach((day) => {
-  container.append(Weekday(day));
-});
+const render = () => {
+  week.forEach((day) => {
+    container.append(Weekday(day));
+  });
+};
+
+render();
