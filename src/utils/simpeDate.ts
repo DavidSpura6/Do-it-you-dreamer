6ebfbe21b3fe1b14851e2d35b4d55d19a130d41f
David Špůra <davidspura6@gmail.com>
2023-02-25 12:21:39 +0100

    Add more date functions

--- src/utils/simpeDate.ts
+++ src/utils/simpeDate.ts
@@ -1,2 +1,13 @@
+import { getCurrentWeek } from "./weekdays";
+
 export const getSimpleDate = (date: Date) =>
   `${date.getFullYear()}_${date.getMonth() + 1}_${date.getDate()}`;
+
+export const getDateFromSimpleDate = (date: string) =>
+  new Date(date.replaceAll("_", " "));
+
+export const getWeekDayNameFromSimpleDate = (date: string) => {
+  const week = getCurrentWeek();
+  const day = week.find((day) => getSimpleDate(day.date) === date);
+  return day!.name;
+};
